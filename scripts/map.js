08b1a21375c9d0f36520f77445b63ac52a191bb3
Mathias Rong <mathiasrong@gmail.com>
2018-08-17 09:00:15 +0200

    OOP

--- scripts/map.js
+++ scripts/map.js
@@ -1,63 +1,88 @@
-$(document).ready(function () {
+function MapComponent(){
+
+  this.map = L.map('map').setView([0, 0], 2);
+
+  // initialize map
+  L.control.scale({
+    imperial: false
+  }).addTo(this.map);
+
+  //load satellite tiles
+  L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+    minZoom: 2,
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+  }).addTo(this.map);
 
 
-	//Initialize Map
-	var map = L.map('map').setView([0, 0], 2);
-
-
-
-	//add scale bar
-	L.control.scale({
-		imperial: false
-	}).addTo(map);
-
-	//Worm Icon 
-	var icon = L.icon({
+	//Worm Icon   TODO: does not work
+	this.default_icon = L.icon({
 		iconUrl: 'http://www.clker.com/cliparts/S/R/r/V/4/k/earth-worm-outline-md.png',
+    //iconUrl: '../assets/images/earth-worm-outline-md.png',
 		iconSize: [50, 35]
 	});
 
+  //group points close to eachother
+  this.clusterGroup = new L.markerClusterGroup().addTo(this.map);
 
-	//Use worm icon
-	function Mark(feature, latlng) {
+  //Use worm icon
+	this.default_marker = function(feature, latlng) {
+    var icon = this.default_icon;
+    if(icon === undefined){
+      log("Icon is undefined!");
+    }
+    if(latlng === undefined){
+      log("No coordinates for "+feature)
+    }
 		return L.marker(latlng, {
 			icon: icon
 		});
 	};
 
+  this.clear_markers = function(){
+    if(this.clusterGroup === undefined){
+      log("No clusterGroup defined");
+      this.clusterGroup = new L.markerClusterGroup().addTo(this.map);
+    }
+    this.clusterGroup.clearLayers();
+  };
 
-	//load satellite tiles
-	var SAT_tiles = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-		minZoom: 2,
-		maxZoom: 20,
-		subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-	}).addTo(map);
+  this.build_markers = function(meta_data,selected_ids){
+    this.clear_markers();
+    var marker = this.default_marker;
 
+    if(marker === undefined){
+      log("Could not create marker instance");
+    }
+    var geo_data = L.geoJSON(meta_data, { //Define how points will look like
+      //pointToLayer: marker,
+      filter: function(feature,layer){
+        var sel_nodes = selected_ids;
+        console.log("Filter for "+sel_nodes);
+        if (sel_nodes !== undefined) { //only run, if something is selected in tree
+          for (var i = 0; i < sel_nodes.length; i++) {
+            if (feature.properties.Symbiont == sel_nodes[i]) return true; //return true if genus fits to selected node in tree
+          }
+        }
+      }
+    });
+    this.clusterGroup.addLayer(geo_data);
+    this.clusterGroup.addTo(this.map); //Actually show points on map
+  }
 
+  // return/export the built instance of the component
+  return this;
 
-	//group points close to eachother
-	var clusterGroup = new L.markerClusterGroup();
+}
 
+// GLOBAL STUFF
+var map_component; //var for global map_component
 
-
-		//Filter points from test_meta.js, called meta_data, according to selected nodes, sel_nodes, from tree
-		function tree_filter(feature, layer) {
-			clusterGroup.clearLayers(); //first remove old points
-			if (sel_nodes !== undefined) { //only run, if something is selected in tree
-				for (var i = 0; i < sel_nodes.length; i++) {
-					if (feature.properties.genus == sel_nodes[i]) return true; //return true if genus fits to selected node in tree
-				}
-			};
-	
-			geo_data = L.geoJSON(meta_data, { //Define how points will look like
-				pointToLayer: Mark,
-				//filter: tree_filter
-			})
-			clusterGroup.addLayer(geo_data).addTo(map); //Actually show points on map
-		};
-
-
-
-
-
+$(document).ready(function () {
+  map_component = new MapComponent(map);
 });
+
+function log(message){
+  alert(message);
+  console.log(message);
+}
--- scripts/tree.js
+++ scripts/tree.js
@@ -1,30 +1,57 @@
-var tree = Phylocanvas.createTree('tree');
+var map;
+var tree_component;
+
+// Create an tree component object
+function TreeComponent(data_path){
+  this.tree = Phylocanvas.createTree('tree'),
+  this.sel_nodes = [];
+
+  this.test = function(str){
+    console.log("Test: "+str+"| sel_nodes = "+this.sel_nodes);
+  };
+
+  this.initialize = function(){
+    this.tree.load(data_path); //load tree data from file
+    //Fehler: XML Parsing Error: syntax error. Klappt aber trotzdem
+
+    this.tree.setTreeType('rectangular'); //define type of tree
+
+    //tree.disableZoom = true;
+
+    this.tree.setNodeSize(5); //
+
+    //tree.highlightColour = 'rgba(249,51,45,1)';
+    //tree.selectedColour = 'rgba(249,51,45,1)';
+
+    //tree.hoverLabel = false; //TODO Klappt nicht, soll vermeiden, dass labes gezeigt werden, wenn über nodes geschwebt wird
+  }
 
 
-tree.load('data/test_tree.nwk'); //load tree data from file
-//Fehler: XML Parsing Error: syntax error. Klappt aber trotzdem
 
-tree.setTreeType('rectangular'); //define type of tree
+  this.tree.on('updated', ({ //when flags ("selected") for nodes change, save all selected node IDs to sel_nodes and run filter function to render point son map
+  	property,
+  	nodeIds
+  }) => {
+  	if (property === 'selected') {
+  		this.sel_nodes = nodeIds;
+      this.test("NODES");
+      handleSelection(nodeIds);
+  		//console.log(sel_nodes);
+  		//tree_filter();
+  	}
+  });
 
+  this.initialize();
 
+}
 
-//tree.disableZoom = true;
+handleSelection = function(ids){
+  //TODO: meta_data is coming from a global js file
+  map.build_markers(meta_data,ids);
+};
 
-tree.setNodeSize(5); //
-
-//tree.highlightColour = 'rgba(249,51,45,1)';
-//tree.selectedColour = 'rgba(249,51,45,1)';
-
-//tree.hoverLabel = false; //TODO Klappt nicht, soll vermeiden, dass labes gezeigt werden, wenn über nodes geschwebt wird
-
-
-tree.on('updated', ({ //when flags ("selected") for nodes change, save all selected node IDs to sel_nodes and run filter function to render point son map
-	property,
-	nodeIds
-}) => {
-	if (property === 'selected') {
-		var sel_nodes = nodeIds;
-		console.log(sel_nodes);
-		tree_filter();
-	}
+$(document).ready(function () {
+  map = map_component;
+  tree_component = new TreeComponent('data/test_tree.nwk');
+  tree_component.test("hui");
 });
