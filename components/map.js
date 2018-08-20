32a98ed188c9964e3cf0a6ea0135fe8f46f1bca1
Mathias Rong <mathiasrong@gmail.com>
2018-08-20 09:44:31 +0200

    fixed worm icon

--- components/map.js
+++ components/map.js
@@ -1,7 +1,7 @@
 function MapComponent(){
 
   this.map = L.map('map').setView([0, 0], 2);
-
+  this.default_icon;
   // initialize map
   L.control.scale({
     imperial: false
@@ -14,20 +14,16 @@ function MapComponent(){
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
   }).addTo(this.map);
 
-
-	//Worm Icon   TODO: does not work
-	this.default_icon = L.icon({
-		iconUrl: 'http://www.clker.com/cliparts/S/R/r/V/4/k/earth-worm-outline-md.png',
-    //iconUrl: '../assets/images/earth-worm-outline-md.png',
-		iconSize: [50, 35]
-	});
-
   //group points close to eachother
   this.clusterGroup = new L.markerClusterGroup().addTo(this.map);
 
   //Use worm icon
 	this.default_marker = function(feature, latlng) {
-    var icon = this.default_icon;
+    var icon = L.icon({
+  		iconUrl: 'http://www.clker.com/cliparts/S/R/r/V/4/k/earth-worm-outline-md.png',
+      //iconUrl: '../assets/images/earth-worm-outline-md.png',
+  		iconSize: [50, 35]
+  	});
     if(icon === undefined){
       log("Icon is undefined!");
     }
@@ -55,7 +51,7 @@ function MapComponent(){
       log("Could not create marker instance");
     }
     var geo_data = L.geoJSON(meta_data, { //Define how points will look like
-      //pointToLayer: marker,
+      pointToLayer: marker,
       filter: function(feature,layer){
         var sel_nodes = selected_ids;
         console.log("Filter for "+sel_nodes);
